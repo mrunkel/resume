184e176ee7812244e862eca7886a03a8c6735155
Marc Runkel <mrunkel@untangle.com>
2017-03-24 17:13:20 +0100

    Working pull script.

--- pull.php
+++ pull.php
@@ -2,19 +2,14 @@
 
 openlog('resume-pull', LOG_NDELAY, LOG_USER);
 
-$secret = $_ENV('GITHUB_SECRET');
+$secret = getenv('GITHUB_SECRET');
 $body = file_get_contents('php://input');
-$hook = json_decode($body, true);
+$hash = "sha1=" . hash_hmac ('sha1', $body, $secret);
 syslog(LOG_NOTICE, "URL called!");
-syslog(LOG_NOTICE, "hook['hook'] = " . print_r($hook['hook'],true));
-syslog(LOG_NOTICE, "hook['hook']['config'] = " . print_r($hook['hook']['config'],true));
-syslog(LOG_NOTICE, "hook['hook']['config']['secret'] = " . $hook['hook']['config']['secret']);
 
-if ($secure) {
+if ($hash === $_SERVER['HTTP_X_HUB_SIGNATURE']) {
   $output = shell_exec ('cd /usr/share/nginx/html/resume && git pull');
-  syslog(LOG_NOTICE, "Command executed, returned " . $output);
+  syslog(LOG_NOTICE, "Command executed, returned: " . $output);
 } else {
   syslog(LOG_ERR, "didn't find the secret!");
 }
-
-  
